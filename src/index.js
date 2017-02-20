e0c60a7df9a1cc485c9ac5f49ecf4e2cd4bad0e2
Ollie Senduk <ollie_senduk@hotmail.com>
2017-02-20 09:55:41 +0100

    AddVideoPlayer

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss';
 
-const API_KEY = 'AIzaSyASmFcxp0E7yKABNeGzlgIdFGXcNpCpTXM';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { videos: [] };
 
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
 
-// Create a new component. This component produces HTML
-const App = () => {
-  return <div> Hello World </div>;
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-// Specify that react will put the component's
-// generated HTML in the DOM
-// Select an instance of the App class
- // and specify where in the DOM you want to render this
 ReactDOM.render(<App />, document.querySelector('.container'));
